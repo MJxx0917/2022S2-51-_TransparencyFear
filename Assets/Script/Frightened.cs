4beb838ddd5383cb58bad63d9c3b677f591487ed
WhatIsTheCoding <85963065+MJxx0917@users.noreply.github.com>
2022-10-25 16:19:40 +1300

    Update Frightened.cs

--- Assets/Script/Frightened.cs
+++ Assets/Script/Frightened.cs
@@ -5,15 +5,17 @@ using UnityEngine.UI;
 
 public class Frightened : MonoBehaviour
 {
+    int amount = 0;
     [SerializeField] private Image image;
 
     //The player walking somewhere will trigger the picture popup event.
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && amount == 0)
         {
             image.enabled = true;
 			GetComponent<AudioSource>().Play();
+			amount++;
 		}
     }
 
